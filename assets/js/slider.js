2af078a3ed968942ccdf30a2b9506a434605c039
webmaster444 <jlmobile710@gmail.com>
2018-09-13 20:38:20 +0300

    slider bug fixed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -74,7 +74,7 @@
   slider_g.append('line').attr('class','white_line').attr('x1',x0).attr('y1',y0).attr('x2',x2).attr('y2',y2).attr('stroke',widget_back).attr('stroke-width','3px');
   var bCurve = slider_g.append("path")
   // .attr("d", "M "+ x0+","+y0 +" Q "+x1 +','+y1 + ' '+x2+','+y2)
-  .attr("d", "M 0,55 H 5 Q 40,30 80,55 H 5")
+  .attr("d", "M 0,55 H 5 Q 40,30 75,55 H 80")
   .attr("stroke", lineColor)
   .attr("stroke-width", "2px")
   .attr("fill", widget_back)
