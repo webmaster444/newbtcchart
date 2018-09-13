ba05c3abd86eb5c0ce68dc9ac4b03d74996030fe
webmaster444 <jlmobile710@gmail.com>
2018-09-13 19:11:09 +0300

    light version added

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,7 +3,17 @@
   height = 120 - margin.top - margin.bottom;
   var prevPeriod = "1y";
   var cuPeriod;
-  var widget_back = '#2C2D37';
+  
+  if(theme_version=='dark'){
+    var lineColor = '#B0B0B4';  
+    var slideTxtColor = 'white';
+    var widget_back = '#2C2D37';
+  }else if(theme_version =='light'){
+    var lineColor = '#340667';
+    var slideTxtColor = '#190430';
+    var widget_back = 'white';
+  }
+  
   var svg = d3.select("#slider").append("svg")
   .attr('viewBox','0 0 '+ (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))  
   .attr("preserveAspectRatio", "xMinYMin meet")
@@ -29,48 +39,48 @@
     function dragmove() {      
       slider.select('.start_period').classed('hide',false);
       slider.select('.end_period').classed('hide',false);
-      var x = d3.event.x - 40;
-      if(x < 0){
-        x = 0;                
+      var xPos = d3.event.x - 40;
+      if(xPos < 0){
+        xPos = 0;                
         slider.select('.start_period').classed('hide',true);
-      }else if(x+80>width){
-        x = width - 80;                   
+      }else if(xPos+80>width){
+        xPos = width - 80;                   
         slider.select('.end_period').classed('hide',true);
       }      
       y0 = 55;      
-      d3.selectAll('.slider_g').attr('transform','translate('+x+',0)');
-      updatePeriod(x);
+      d3.selectAll('.slider_g').attr('transform','translate('+xPos+',0)');
+      updatePeriod(xPos);
     };
 
   var slider = svg.append('g').attr('class','slider').attr('transform','translate('+margin.left + ',' + margin.top + ')');
-  slider.append('line').attr("x1", 0).attr('class','main_line').attr('y1',y0).attr("x2", width).attr('y2',y0).attr('stroke','#B0B0B4').attr('stroke-width','2px').on('mousedown',function(d){                                
-                    var x = d3.mouse(this)[0] - 40;
-                    if(x+80 >width){
-                      x = width - 80;
+  slider.append('line').attr("x1", 0).attr('class','main_line').attr('y1',y0).attr("x2", width).attr('y2',y0).attr('stroke',lineColor).attr('stroke-width','2px').on('mousedown',function(d){                                
+                    var xPos = d3.mouse(this)[0] - 40;
+                    if(xPos+80 >width){
+                      xPos = width - 80;
                       slider.select('.end_period').classed('hide',true);
-                    }else if(x - 40 < 0){
-                      x = 0;
+                    }else if(xPos - 40 < 0){
+                      xPos = 0;
                       slider.select('.start_period').classed('hide',true);
                     }
-                    d3.selectAll('.slider_g').transition().duration(2000).attr('transform','translate('+x+',0)');
-                    updatePeriod(x,true);                    
+                    d3.selectAll('.slider_g').transition().duration(2000).attr('transform','translate('+xPos+',0)');
+                    updatePeriod(xPos,true);                    
   });  
-  slider.append('circle').attr('cx',0).attr('cy',y0).attr('r',2).attr('fill','#B0B0B4');
-  slider.append('circle').attr('cx',width).attr('cy',y0).attr('r',2).attr('fill','#B0B0B4');
-  slider.append('text').attr('x',0).attr('y',y0 - 30).text('Slider to adjust').attr('fill','white').attr('class','font_bold').style('opacity','.4').style('font-size','12px');
+  slider.append('circle').attr('cx',0).attr('cy',y0).attr('r',2).attr('fill',lineColor);
+  slider.append('circle').attr('cx',width).attr('cy',y0).attr('r',2).attr('fill',lineColor);
+  slider.append('text').attr('x',0).attr('y',y0 - 30).text('Slider to adjust').attr('fill',slideTxtColor).attr('class','font_bold').style('opacity','.6').style('font-size','12px');
   slider.append('text').attr('x',0).attr('y',y0 + 30).text('1 year').attr('class','start_period hide').attr('fill','#97979C');
   slider.append('text').attr('x',width).attr('y',y0 + 30).text('1 week').attr('class','end_period').attr('fill','#97979C').attr('text-anchor','end');
   var slider_g = slider.append('g').attr('class','slider_g').attr('transform','translate(0,0)').call(drag);
   slider_g.append('line').attr('class','white_line').attr('x1',x0).attr('y1',y0).attr('x2',x2).attr('y2',y2).attr('stroke',widget_back).attr('stroke-width','2px');
   var bCurve = slider_g.append("path")
   // .attr("d", "M "+ x0+","+y0 +" Q "+x1 +','+y1 + ' '+x2+','+y2)
-  .attr("d", "M 0,55 H 5 Q 40,30 80,55 H 5")
-  .attr("stroke", "#B0B0B4")
+  .attr("d", "M 0,55 H 5 Q 40,30 80,55")
+  .attr("stroke", lineColor)
   .attr("stroke-width", "2px")
   .attr("fill", widget_back)
   // .call(drag);
 
-  slider_g.append('text').attr('class','label_text').attr('x',x1).attr('y',y0 + 10).attr('fill','#EEEEEF').text('1 year').attr('text-anchor','middle').style('font-size','13px')
+  slider_g.append('text').attr('class','label_text').attr('x',x1).attr('y',y0 + 10).attr('fill',slideTxtColor).text('1 year').attr('text-anchor','middle').style('font-size','13px')
   // .call(drag);
   slider_g.append('circle').attr('class','label_circle').attr('cx',x1).attr('cy',y1 + 50).attr('r',8).attr('fill','#24D17A')
   // .call(drag);  
@@ -146,20 +156,20 @@
     var w1Period = svgWidth //1w
     switch(period){
       case "1y":
-        x = 0;
+        xPos = 0;
       break;
       case "6m":
-        x = yPeriod;
+        xPos = yPeriod;
       break;
       case "1m":
-        x = m6Period;
+        xPos = m6Period;
       break;
       case "2w":
-        x = m1Period;
+        xPos = m1Period;
       break;
       case "1w":
-        x = w2Period;
+        xPos = w2Period;
       break;      
     }
-    d3.selectAll('.slider_g').transition().duration(2000).attr('transform','translate('+x+',0)');
+    d3.selectAll('.slider_g').transition().duration(2000).attr('transform','translate('+xPos+',0)');
   }
